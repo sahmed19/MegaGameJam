c1656067f5bb42b7ffb1e342bec7007fbfb33ff6
Aidan Takami <aidan.takami@yahoo.com>
2019-10-26 01:13:09 -0700

    Pause Menu UI 0.5

---
+++ MegaGameJamProject/Assets/Scripts/Interface/PauseMenuScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUi;
+
+
+    private void Start()
+    {
+
+        pauseMenuUi.SetActive(false);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+
+    }
+
+}
