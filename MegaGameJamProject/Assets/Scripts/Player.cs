b9cdf9532a1d090991ac8e9ce8b4bf26148da31d
Aidan Takami <aidan.takami@yahoo.com>
2019-10-25 20:56:14 -0700

    Player & Enemy HP
    Attack & HP

---
+++ MegaGameJamProject/Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    //Health Bar
+    public Slider healthSlider;
+
+
+    /*Damage Image & vars
+
+    public ImageConversion damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    */
+
+        //Dead or Damaged bools
+    bool isDead;
+    bool damaged;
+
+        //Establishes Player Health
+    void Awake()
+    {
+
+        currentHealth = startingHealth;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //If Player takes damage
+        if (damaged)
+        {
+            // damageImage.color = flashColor
+        }
+        else
+        {
+            //damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        //Reset damaged flad
+        damaged = false;
+
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        //Flag Damage
+        damaged = true;
+
+        //Damage taken
+        currentHealth -= amount;
+
+        //Health Slider
+        healthSlider.value = currentHealth;
+
+        //Player dies
+        if (currentHealth <= 0 && !isDead) Death();
+
+    }
+
+    //Death Method
+    void Death()
+    {
+
+        isDead = true;
+
+        //Turn off player movement
+        //Death sequence
+
+
+    }
+
+
+}
---
+++ MegaGameJamProject/Assets/Scripts/UpperEnemyAtk.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpperEnemyAtk : MonoBehaviour
+{
+
+    //Enemy Attack fields
+    public float timeBetweenAttacks;
+    public int attackDamage;
+
+
+    //Reference to playerHealth in Player
+    Player playerHealth;
+
+    //Reference to Enemy HP in UpperEnemyHP
+    UpperEnemyHP enemyHealth;
+
+    //Reference to GO player
+    GameObject player;
+
+    //In Range?
+    bool playerInRange;
+
+    //Attack timer
+    float timer;
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<Player>();
+        enemyHealth = GetComponent<UpperEnemyHP>();
+
+    }
+
+    //Player pull enemy
+    void OnTriggerEnter(Collider other)
+    {
+        //COLLIDER for the enemy range
+        if (other.gameObject == player)
+        {
+            //Player is within X ft
+            playerInRange = true;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+        // If the timer exceeds the time between attacks, the player is in range and this enemy is alive
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // Reset the timer.
+        timer = 0f;
+
+        // If the player has health to lose...
+        if (playerHealth.currentHealth > 0)
+        {
+            // ... damage the player.
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+
+}
