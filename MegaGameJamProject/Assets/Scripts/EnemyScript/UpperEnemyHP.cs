b6389eb8dd907f5aefb279af7a708d90a12db8c8
Aidan Takami <aidan.takami@yahoo.com>
2019-10-25 20:59:07 -0700

    Player & Enemy HP 2.0

---
+++ MegaGameJamProject/Assets/Scripts/EnemyScript/UpperEnemyHP.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpperEnemyHP : MonoBehaviour
+{
+
+    //Health values
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    //Enemy is dead?
+    bool isDead;
+
+    void Awake()
+    {
+        //Equate HP
+        currentHealth = startingHealth;
+    }
+
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        // If the enemy = dead af
+        if (isDead) return;
+
+        // Take hit
+        currentHealth -= amount;
+
+        // If enemy dies
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        // The enemy is dead.
+        isDead = true;
+       
+        // After 2 seconds destory the enemy.
+        Destroy(gameObject, 2f);
+        
+        // Enemy cant be hit??
+        //  capsuleCollider.isTrigger = true;
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
