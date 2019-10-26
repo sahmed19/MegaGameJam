c4983f713eda315e0563942f681aee4c874a10e0
StressoBoi <38673935+Taygh@users.noreply.github.com>
2019-10-26 15:45:47 -0700

    Polished popups

--- MegaGameJamProject/Assets/Scripts/Interface/PopUp.cs
+++ MegaGameJamProject/Assets/Scripts/Interface/PopUp.cs
@@ -7,24 +7,53 @@ public class PopUp : MonoBehaviour
 {
     [SerializeField] Text popUpText;
     [SerializeField] Vector3 offset;
+    [SerializeField] GameObject player;
+    [SerializeField] float fadeSpeed;
+    [SerializeField] bool destroyOnClick;
+    [SerializeField] bool destroyOnKey;
+    [SerializeField] string key;
 
     void Start()
     {
         TextTransparencyReset();
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && destroyOnClick)
+        {
+            DestroyText();
+        }
+
+        if(key != "")
+        {
+            if (Input.GetKeyDown(key) && destroyOnKey)
+            {
+                DestroyText();
+            }
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
         popUpText.transform.position = collision.gameObject.transform.position + offset;
-        //popUpText.transform.parent = collision.gameObject.transform;
+        popUpText.CrossFadeAlpha(100, fadeSpeed, false);
         popUpText.transform.SetParent(collision.gameObject.transform);
-        popUpText.CrossFadeAlpha(100, 100f, false);
     }
 
     void TextTransparencyReset()
     {
         popUpText.CrossFadeAlpha(0, 0f, false);
+    }
+
+    void DestroyText()
+    {
+        if(popUpText != null)
+        {
+            popUpText.transform.parent = null;
+            Destroy(popUpText);
+        }
     }
 }
 
