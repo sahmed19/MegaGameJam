5791707518c32b321c0eee165d6d1cfafee07561
Aidan Takami <aidan.takami@yahoo.com>
2019-10-26 11:50:16 -0700

    lowpassing

--- MegaGameJamProject/Assets/Scripts/Interface/AudioManager.cs
+++ MegaGameJamProject/Assets/Scripts/Interface/AudioManager.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
+    AudioLowPassFilter lowPassFilter;
+
     //Preserves Audio Source
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        lowPassFilter = GetComponent<AudioLowPassFilter>();
+
+
+    }
+
+    private void Update()
+    {
+        if(Player.INSTANCE != null)
+        {
+            lowPassFilter.cutoffFrequency = Player.INSTANCE.PlayerInUnderworld() ? 1000 : 5000;
+        }
     }
 }
