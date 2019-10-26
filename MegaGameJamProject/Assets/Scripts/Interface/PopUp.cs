ff6ceb50d30352d3c8b5de19375fdf8ca6cc73b6
StressoBoi <38673935+Taygh@users.noreply.github.com>
2019-10-26 14:49:21 -0700

    Added scripts for popup and refined statue, added level elements, slight changes to tileset

---
+++ MegaGameJamProject/Assets/Scripts/Interface/PopUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUp : MonoBehaviour
+{
+    [SerializeField] Text popUpText;
+    [SerializeField] Vector3 offset;
+
+    void Start()
+    {
+        TextTransparencyReset();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+        popUpText.transform.position = collision.gameObject.transform.position + offset;
+        //popUpText.transform.parent = collision.gameObject.transform;
+        popUpText.transform.SetParent(collision.gameObject.transform);
+        popUpText.CrossFadeAlpha(100, 100f, false);
+    }
+
+    void TextTransparencyReset()
+    {
+        popUpText.CrossFadeAlpha(0, 0f, false);
+    }
+}
+
