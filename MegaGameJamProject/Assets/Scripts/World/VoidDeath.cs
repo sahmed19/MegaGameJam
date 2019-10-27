5c616ab6d14c7526f99ab71a287b330e40db520f
StressoBoi <38673935+Taygh@users.noreply.github.com>
2019-10-27 08:57:49 -0700

    Added void death

---
+++ MegaGameJamProject/Assets/Scripts/World/VoidDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class VoidDeath : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            Debug.Log("hit");
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<TilemapCollider2D>());
+
+            StartCoroutine(WaitForDeath(0.8f));
+    }
+
+    IEnumerator WaitForDeath(float f)
+    {
+
+        yield return new WaitForSeconds(f);
+        player.GetComponent<Player>().Death();
+    }
+}
